645505000e1fb60ae3ba0818cf2527827f7c1c6c
Ryan Andrade <00001098143966sp@al.educacao.sp.gov.br>
2025-09-23 21:37:23 -0300

    Update exercicios_aula11.sql

--- exercicios_aula11.sql
+++ exercicios_aula11.sql
@@ -13,4 +13,30 @@ group by a.id_autor;
 select a.nome as autor, avg(l.paginas) as media_paginas
 from autor a
 join livro l on a.id_autor = l.id_autor
+
 group by a.id_autor;
+
+-- Listar o total de campanhas por reservatório (limnologia_db)
+
+select r.nome as reservatorio, count(c.id_campanha) as total_campanhas
+from reservatorio r
+inner join campanha c on r.id_reservatorio = c.id_reservatorio
+group by r.nome
+order by total_campanhas desc;
+
+-- Mostrar a média de valores de cada parâmetro em séries temporais (limnologia_db)
+
+select p.nome_parametro, AVG(s.valor) as media_valores
+from serie_temporal s
+inner join parametro p on s.id_parametro = p.id_parametro
+group by p.nome_parametro
+order by media_valores desc;
+
+-- Exibir apenas as instituições que realizaram mais de 3 campanhas (limnologia_db)
+
+select i.nome as instituicao, count(c.id_campanha) as total_campanhas
+from instituicao i
+inner join campanha c on i.id_instituicao = c.id_instituicao
+group by i.nome
+having count(c.id_campanha) > 3
+order by total_campanhas desc;
